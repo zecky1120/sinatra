0be4584baf414e5a4cf6fa4c9123ef82996c04dd
zecky1120 <takahide1120@gmail.com>
2024-03-13 11:15:53 +0900

    add: メモ作成の構築

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
-
+require 'securerandom'
 
 # ------------------------- method
 def files
@@ -24,9 +24,24 @@ get '/' do
   redirect '/memos'
 end
 
+get '/memos' do
+  memos = files.map {|file| get_memo(file)}
+  @memos = memos.sort_by {|f| f['time']}
+  @title = 'トップ'
+  erb :index
+end
+
 # new
 get '/memos/new' do 
   @title = '入力画面'
   erb :new
 end
 
+# create
+post '/memos' do
+  memo = {'id' => SecureRandom.hex(10), 'title' => params['title'], 'content' => params['content'], 'time' => Time.now }
+  File.open("./data/#{memo['id']}.json", 'w') do |f|
+    JSON.dump(memo, f)
+  end
+  redirect "/memos"
+end
