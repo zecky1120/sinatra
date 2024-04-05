13bb4036f91f1339078360d9b1a569fef83a06bb
zecky1120 <takahide1120@gmail.com>
2024-04-05 17:08:23 +0900

    fix: memo.rb,main.rbの修正

--- main.rb
+++ main.rb
@@ -4,39 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 require 'securerandom'
-# require_relative 'memo'
+require_relative 'memo'
 
-TABLE_NAME = 'memos'
-DB_NAME = 'memo_app'
-CONN = PG::Connection.new(dbname: "#{DB_NAME}")
-
-def build_id
-  SecureRandom.hex
-end
-
-def memos
-  query = "SELECT * FROM #{TABLE_NAME}"
-  CONN.exec(query)
-end
-
-def get_memo(id)
-  memos.find { |memo| memo['id'] == id }
-end
-
-def create_memo(id, title, content, created_at)
-  query = "INSERT INTO #{TABLE_NAME}(id, title, content, created_at) VALUES ($1, $2, $3, $4);"
-  CONN.exec_params(query, [id, title, content, created_at])
-end
-
-def update_memo(id, title, content)
-  query = "UPDATE #{TABLE_NAME} SET title = $1, content = $2 WHERE id = $3;"
-  CONN.exec_params(query, [title, content, id])
-end
-
-def delete_memo(id)
-  query = "DELETE FROM #{TABLE_NAME} WHERE id = $1;"
-  CONN.exec_params(query, [id])
-end
+memo = Memo.new
 
 helpers do
   def h(text)
@@ -49,7 +19,7 @@ get '/' do
 end
 
 get '/memos' do
-  @memos = memos.sort_by { |memo| memo['created_at'] }
+  @memos = memo.all
   @title = 'トップ'
   erb :index
 end
@@ -60,16 +30,16 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  id = build_id
+  id = memo.build_id
   title = params['title']
   content = params['content']
   created_at = Time.now
-  create_memo(id, title, content, created_at)
+  memo.create(id, title, content, created_at)
   redirect "/memos"
 end
 
 get '/memos/:id' do |id|
-  @memo = get_memo(id)
+  @memo = memo.show(id)
   if @memo
     @title = @memo['title']
     erb :show
@@ -79,7 +49,7 @@ get '/memos/:id' do |id|
 end
 
 get '/memos/:id/edit' do |id|
-  @memo = get_memo(id)
+  @memo = memo.show(id)
   @title = "編集 - #{@memo['title']}"
   erb :edit
 end
@@ -87,12 +57,12 @@ end
 patch '/memos/:id' do |id|
   title = params['title']
   content = params['content']
-  update_memo(id, title, content)
+  memo.update(id, title, content)
   redirect "/memos/#{id}"
 end
 
 delete '/memos/:id' do |id|
-  delete_memo(id)
+  memo.delete(id)
   redirect '/memos'
 end
 
--- memo.rb
+++ memo.rb
@@ -1,7 +1,38 @@
 # frozen_string_literal: true
 
+DB_NAME = 'memo_app'
+TABLE_NAME = 'memos'
+CONN = PG::Connection.new(dbname: "#{DB_NAME}")
+
 class Memo
-  def initialize
-    @conn = PG::Connection.new(dbname: 'memos')
+
+  def build_id
+    SecureRandom.hex
   end
+  
+  def all
+    query = "SELECT * FROM #{TABLE_NAME}"
+    memos = CONN.exec(query)
+    memos.sort_by { |memo| memo['created_at'] }
+  end
+
+  def show(id)
+    all.find { |memo| memo['id'] == id }
+  end
+
+  def create(id, title, content, created_at)
+    query = "INSERT INTO #{TABLE_NAME}(id, title, content, created_at) VALUES ($1, $2, $3, $4);"
+    CONN.exec_params(query, [id, title, content, created_at])
+  end
+
+  def update(id, title, content)
+    query = "UPDATE #{TABLE_NAME} SET title = $1, content = $2 WHERE id = $3;"
+    CONN.exec_params(query, [title, content, id])
+  end
+  
+  def delete(id)
+    query = "DELETE FROM #{TABLE_NAME} WHERE id = $1;"
+    CONN.exec_params(query, [id])
+  end
+
 end
