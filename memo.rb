6d0c3c98cc8a17c7d24d9fbe8fc480f0db5a4a6a
zecky1120 <takahide1120@gmail.com>
2024-04-08 16:11:10 +0900

    fix: findメソッドの修正

--- memo.rb
+++ memo.rb
@@ -5,18 +5,15 @@ CONN = PG::Connection.new(dbname: 'memo_app')
 # Memo class(index, show, create, edit, delete etc.)
 class Memo
   class << self
-    # def build_id
-    #   SecureRandom.hex
-    # end
 
     def all
-      query = "SELECT * FROM memos"
-      memos = CONN.exec(query)
-      memos.sort_by { |memo| memo['created_at'] }
+      query = "SELECT * FROM memos ORDER BY id ASC;"
+      CONN.exec(query)
     end
 
     def find(id)
-      all.find { |memo| memo['id'] == id }
+      query = "SELECT * FROM memos WHERE id = $1"
+      CONN.exec(query, [id]).first
     end
 
     def create(title, content, created_at)
