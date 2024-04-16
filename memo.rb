af325d6c14620d3659ea7a14390232be3d46de77
zecky1120 <takahide1120@gmail.com>
2024-04-16 11:43:13 +0900

    fix: 定数CONNををprivateメソッド内で変数化

--- memo.rb
+++ memo.rb
@@ -5,27 +5,27 @@ class Memo
   class << self
     def all
       query = 'SELECT * FROM memos ORDER BY id ASC'
-      @conn.exec(query).to_a
+      conn.exec(query).to_a
     end
 
     def find(id)
       query = 'SELECT * FROM memos WHERE id = $1'
-      @conn.exec(query, [id]).first
+      conn.exec(query, [id]).first
     end
 
     def create(params)
       query = 'INSERT INTO memos(title, content, created_at) VALUES ($1, $2, $3)'
-      @conn.exec_params(query, params.values_at('title', 'content', 'created_at'))
+      conn.exec_params(query, params.values_at('title', 'content', 'created_at'))
     end
 
     def update(params)
       query = 'UPDATE memos SET title = $2, content = $3 WHERE id = $1'
-      @conn.exec_params(query, params)
+      conn.exec_params(query, params)
     end
 
     def delete(id)
       query = 'DELETE FROM memos WHERE id = $1'
-      @conn.exec_params(query, [id])
+      conn.exec_params(query, [id])
     end
 
     private
